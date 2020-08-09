21fe34b367bc042c4cc297de25fd0f51a4790e4f
makey.yang <1007845131@qq.com>
2020-08-09 15:47:17 +0800

    func & map

---
+++ week08/map.py
@@ -0,0 +1,21 @@
+class map(object):
+
+    def __getattribute__(self, *args, **kwargs):
+        pass
+
+    def __init__(self, func,
+                 *iterables):
+        pass
+
+    def __iter__(self, *args, **kwargs):
+        pass
+
+    @staticmethod
+    def __new__(*args, **kwargs):
+        pass
+
+    def __next__(self, *args, **kwargs):
+        pass
+
+    def __reduce__(self, *args, **kwargs):
+        pass
